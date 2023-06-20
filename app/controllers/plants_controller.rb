d022dedf292de73f7a8082a883ff73558cce8559
brianrono <brianrono16@gmail.com>
2023-06-20 23:19:24 +0300

    Complete

--- app/controllers/plants_controller.rb
+++ app/controllers/plants_controller.rb
@@ -3,25 +3,20 @@ class PlantsController < ApplicationController
         plants = Plant.all
         render json: plants
     end
-    
+
     def show
         plant = Plant.find(params[:id])
         render json: plant
     end
-    
+
     def create
-        plant = Plant.new(plant_params)
-    
-        if plant.save
-            render json: plant, status: :created
-        else
-            render json: plant.errors, status: :unprocessable_entity
-        end
+        plant = Plant.create(plant_params)
+        render json: plant, status: :created
     end
-    
+
     private
-    
+
     def plant_params
-        params.require(:plant).permit(:name, :image, :price)
+        params.permit(:name, :image, :price)
     end
 end
